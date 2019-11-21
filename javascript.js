e4911ea652934d20ffd077b0eed5b0f912ca1f3b
elifsancak <37196405+elifsancak@users.noreply.github.com>
2019-11-21 23:57:27 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -284,3 +284,43 @@ var myComputer = {
     // My todos: [ "item1", "item2", "item3" ]
     todoList.deleteTodo(1);
     // My todos: [ "item1", "item3" ]
+
+
+
+
+
+// VERSION-4 BOOLEANS
+
+var todoList = {
+todos: [],
+displayTodos: function(){
+console.log("My todos:", this.todos);
+},
+addTodo: function(todoText){
+this.todos.push({
+todoText: todoText,
+completed: false
+});
+this.displayTodos();
+},
+changeTodo: function(position,todoText){
+//this.todos[position] = newValue;
+this.todos[position].todoText = todoText;
+this.displayTodos();
+},
+deleteTodo: function(position){
+todos.splice(position, 1);
+displayTodos();
+},
+toggleCompleted: function(position){
+var todo = this.todos[position];
+todo.completed = !todo.completed;
+this.displayTodos();
+}
+};
+
+
+todoList.addTodo('this is an object');
+todoList.changeTodo(0, 'second try');
+todoList.toggleCompleted(0);
+// My todos: [ {…} ]
